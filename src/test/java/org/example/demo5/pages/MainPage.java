aed2852e731fc5f070ded238890e4cd5fcbf6115
RuslanMRB <ruslanqweewq123@yandex.ru>
2024-07-27 15:31:32 +0300

    Update MainPage.java

--- src/test/java/org/example/demo5/pages/MainPage.java
+++ src/test/java/org/example/demo5/pages/MainPage.java
@@ -1,13 +1,20 @@
 package org.example.demo5.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 //https://www.bing.com/
 
 public class MainPage {
+
+   // private WebDriver driver;
 
     @FindBy(css = "input#sb_form_q")
     private WebElement searchField;
@@ -16,6 +23,11 @@ public class MainPage {
         searchField.sendKeys(text);
         searchField.submit();
         System.out.println("Введен текст: " + text);
+        //  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        // wait.until(ExpectedConditions.and(
+        //       ExpectedConditions.attributeContains(By.cssSelector("h2 > a[href]"), "href", "selenium"),
+        //       ExpectedConditions.elementToBeClickable(By.cssSelector("h2 > a[href]"))
+        //   ));
     }
 
     public MainPage(WebDriver driver){
