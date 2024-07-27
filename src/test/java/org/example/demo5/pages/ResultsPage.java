a7454fdfc703f21aae7f660f2f6e865f0c62da5c
RuslanMRB <ruslanqweewq123@yandex.ru>
2024-07-27 15:31:37 +0300

    Update ResultsPage.java

--- src/test/java/org/example/demo5/pages/ResultsPage.java
+++ src/test/java/org/example/demo5/pages/ResultsPage.java
@@ -1,10 +1,14 @@
 package org.example.demo5.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class ResultsPage {
@@ -12,17 +16,25 @@ public class ResultsPage {
     @FindBy(css = "input#sb_form_q")
     private WebElement searchField;
 
-    @FindBy(css = "h2 > a[href]")
+    @FindBy(css = ":not(.b_adurl) > cite")
     private List<WebElement> results;
 
+    private WebDriver driver;
 
     public void clickElement(int num){
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(":not(.b_adurl) > cite"), "selenium"),
+                ExpectedConditions.elementToBeClickable(By.cssSelector(":not(.b_adurl) > cite"))
+        ));
         results.get(num).click();
         System.out.println("Нажатие на результат под номером " + num);
 
     }
 
     public ResultsPage(WebDriver driver){
+        this.driver = driver;
         PageFactory.initElements(driver, this);
 
     }
